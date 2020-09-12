8a95420a29c545e4c114faed3aa1715c8040b4be
Abhinav Shukla <abhinav2014shukla@gmail.com>
2020-09-12 17:41:02 +0530

    Project_push_#4

--- app/src/main/java/com/example/khatabook/ClickOnMore.java
+++ app/src/main/java/com/example/khatabook/ClickOnMore.java
@@ -2,13 +2,28 @@ package com.example.khatabook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ClickOnMore extends AppCompatActivity {
+    private ImageView buttonInvite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_on_more);
+
+        buttonInvite=findViewById(R.id.buttonInvite);
+
+buttonInvite.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intentForInvite=new Intent(ClickOnMore.this,Invite_page.class);
+        startActivity(intentForInvite);
+    }
+});
+
     }
 }
---
+++ app/src/main/java/com/example/khatabook/Invite_page.java
@@ -0,0 +1,38 @@
+package com.example.khatabook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Invite_page extends AppCompatActivity {
+    private ImageView btClickToInviteIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_page);
+        btClickToInviteIntent=findViewById(R.id.ClickToInviteIntent);
+
+        btClickToInviteIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inviteIntent=new Intent();
+                inviteIntent.setAction(Intent.ACTION_SEND);
+                inviteIntent.putExtra(Intent.EXTRA_TEXT,"http://bit.ly/khatabook-app");
+                inviteIntent.setType("plain/text");
+
+                if(inviteIntent.resolveActivity(getPackageManager() )!= null){
+                    startActivity(inviteIntent);
+                }
+
+            }
+        });
+
+
+
+
+    }
+}
