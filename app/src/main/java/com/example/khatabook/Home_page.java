737caad4c9b442cb23a72d0747fa47a108cf86b1
Abhinav Shukla <abhinav2014shukla@gmail.com>
2020-09-12 16:41:30 +0530

    Project_push_#3

--- app/src/main/java/com/example/khatabook/Home_page.java
+++ app/src/main/java/com/example/khatabook/Home_page.java
@@ -2,13 +2,30 @@ package com.example.khatabook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home_page extends AppCompatActivity {
+    private ImageView btImMoreButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        btImMoreButton=findViewById(R.id.btImMoreButton);
+
+        btImMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(Home_page.this,ClickOnMore.class);
+                startActivity(intent2);
+
+            }
+        });
+
     }
 }
