950eb80fba0474a4d458779fbf2f289b0be0d1a1
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-06-01 11:10:28 +0530

    Streak_Khatabook

--- app/src/main/java/com/example/khatabook/NavigationDrawer.java
+++ app/src/main/java/com/example/khatabook/NavigationDrawer.java
@@ -1,7 +1,4 @@
 package com.example.khatabook;
 
 public class NavigationDrawer {
-    //implement it soon
-    //add some functionality to khatabook app layouts were good
-    //add this app in portfolio and resume
 }
